573c523c1064ea5c3de28322fe7786da0854e384
Fabrício Samuel Lubczyk <fabriciosamuel737@gmail.com>
2026-05-06 12:09:20 -0300

    finish

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const textarea = document.getElementById("add-texto")
 const botaoadd = document.getElementById("botao-add")
 const botaoremove = document.getElementById("botao-remove")
 const botaoremovelast = document.getElementById("botao-remove-last")
+const botaoMaiusculo = document.getElementById("maiusculo")
 
-let lista_nomes = ["Kawan do js", "fabzerasiuuu", "Ûésley", "acerólâ" ]
+let lista_nomes = ["Kawan do js", "fabzerasiuuu", "Ûésley", "acerólâ"]
 
 function renderizar(){
     const lista_pagina = document.getElementById("lista")
@@ -20,13 +21,52 @@ botaoadd.addEventListener('click', function() {
     const nome = textarea.value;
 
     if (nome.trim() !== "") {
-        lista_nomes.push(nome);
+        lista_nomes.unshift(nome);
         renderizar();
         
         textarea.value = '';
         textarea.focus();
     }
 });
+
+botaoremove.addEventListener("click", function(){
+    lista_nomes.shift();
+    renderizar();
+})
+
+botaoremovelast.addEventListener("click", function(){
+    lista_nomes.pop();
+    renderizar();
+})
+
+botaoMaiusculo.addEventListener("click", function(){
+    let resultado = "";
+    lista_nomes.forEach(function(nome){
+        resultado += nome.toUpperCase() + " ";
+    })
+    document.getElementById("result").textContent = resultado;
+})
+
+function contador() {
+    let contador = 0;
+
+    for (let i = 0; i < lista_nomes.length; i++) {
+        if (lista_nomes[i].length > 5) {
+            contador++;
+        }
+    }
+
+    document.getElementById("resultado").textContent =
+        "total de nomes com mais de 5 letras: " + contador;
+}
+
+function filtrar(){
+    const maiores = lista_nomes.filter(function(nome){
+        return nome.length > 5;
+    })
+
+    document.getElementById("res").textContent = maiores.join(", ");
+}
 
 document.addEventListener("DOMContentLoaded", ()=>{
     renderizar()
